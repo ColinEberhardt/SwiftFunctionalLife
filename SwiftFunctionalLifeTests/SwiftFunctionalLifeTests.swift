590f8a10cc8cdf49bdb6e1b87a4368af6b92f44e
Colin Eberhardt <colin.eberhardt@gmail.com>
2014-08-30 10:15:14 +0100

    Initial Commit

---
+++ SwiftFunctionalLifeTests/SwiftFunctionalLifeTests.swift
@@ -0,0 +1,36 @@
+//
+//  SwiftFunctionalLifeTests.swift
+//  SwiftFunctionalLifeTests
+//
+//  Created by Colin Eberhardt on 30/08/2014.
+//  Copyright (c) 2014 Colin Eberhardt. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class SwiftFunctionalLifeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
